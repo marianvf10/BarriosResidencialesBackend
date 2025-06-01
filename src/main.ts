e17581dce8bbe31afc079bc82197be39a0b21e63
Mariano Vergara <mariano.vergara@est.fi.uncoma.edu.ar>
2025-06-01 17:33:08 -0300

    Se setea el prefijo global de la API y tambien se setea el pipeglobal para usar validaciones en los dtos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('api')
+    app.useGlobalPipes(
+    new ValidationPipe({
+       whitelist: true, // elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // lanza error si hay propiedades extra
+      // transform: true, // transforma payloads a instancias de clase
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
