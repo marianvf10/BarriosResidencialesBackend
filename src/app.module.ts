a7648de068845139c82699e1d75ea01ea0890017
Mariano Vergara <mariano.vergara@est.fi.uncoma.edu.ar>
2025-06-01 17:33:23 -0300

    Se agrega la configuracion para conectar la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PlotsModule } from './plots/plots.module';
+import { PersonasModule } from './personas/personas.module';
+import { SeedModule } from './seed/seed.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { UsuarioModule } from './usuario/usuario.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [PlotsModule],
+  imports: [
+    PlotsModule, PersonasModule, SeedModule, 
+    ServeStaticModule.forRoot({rootPath:join(__dirname,'..','public')}), 
+    UsuarioModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/barrios-residenciales-backend'),
+   ],
   controllers: [],
-  providers: [],
+   providers: [],
 })
 export class AppModule {}
