0c47d00f75055cf70b62e97f81236809b1cfb84e
Mariano Vergara <mariano.vergara@est.fi.uncoma.edu.ar>
2025-06-02 20:19:29 -0300

    Se agrego el modulo de commons y dentro de ese se creo el parse mongo id pipe para validar los ids en las peticiones

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UsuarioModule } from './usuario/usuario.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 
 @Module({
@@ -14,6 +15,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     ServeStaticModule.forRoot({rootPath:join(__dirname,'..','public')}), 
     UsuarioModule,
     MongooseModule.forRoot('mongodb://localhost:27017/barrios-residenciales-backend'),
+    CommonModule,
    ],
   controllers: [],
    providers: [],
